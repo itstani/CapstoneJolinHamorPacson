f71332ac41d0f7b9d43172c651fb6bfdd9650975
LagDude <tantan.jolin15@gmail.com>
2024-09-14 16:12:39 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/images/:imageName', (req, res) => {
+  const imageName = req.params.imageName;
+  res.sendFile(__dirname + '/images/' + imageName);
+});
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'Monitoring system', 'register.html'));
+});
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
