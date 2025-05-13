604fa4f8fdf5c755e0230807199cb1aed3cd4c89
itstani <itskuyatani@gmail.com>
2025-05-13 22:44:10 +0800

    abuba

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(protectAdminRoutes);
 module.exports = protectAdminRoutes
 
 // And then update the usage
-app.use(authMiddleware);
+app.use(auth-Middleware);
 // Add this debug middleware right after session middleware
 app.use((req, res, next) => {
   console.log("=== Session Debug Info ===");
